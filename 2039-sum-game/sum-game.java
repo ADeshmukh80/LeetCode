97df66711a045882f9a5ccbb6b1f94f577342483
Anisha Deshmukh <123193995+ADeshmukh80@users.noreply.github.com>
2026-08-23 13:12:18 +0530

    Time: 9 ms (29.93%) | Memory: 47.2 MB (26.28%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean sumGame(String num) {
+         int n = num.length();
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        // Left half
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        // Right half
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        // Odd number of '?'  Alice wins
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        int difference = leftSum - rightSum;
+        int possibleDifference = 9 * (rightQ - leftQ) / 2;
+
+        return difference != possibleDifference;
+    }
+}
